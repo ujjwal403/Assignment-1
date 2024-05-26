49917a39ceac89a2973a34d4cc5a1aab901bb6af
ujjwal403 <tiwariujjwal034@gmail.com>
2024-05-26 14:30:26 +0530

    Add files via upload

---
+++ question-6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {    
+
+    int  A, B , sum;
+    
+    printf("Enter A ,B : ");
+    scanf("%d %d", &A, &B);
+
+    
+    sum = A + B;      
+    
+    printf("%d + %d = %d", A, B , sum);
+    return 0;
+}
